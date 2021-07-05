a2b6275b4f01db45f349a2c22376f6f579dd8db4
RAlanWright <wright.alan88@gmail.com>
2021-07-05 15:08:15 -0500

    Switched to npm LTS and reinstalled gatsby

--- mysite/frontend/gatsby-config.js
+++ mysite/frontend/gatsby-config.js
@@ -33,16 +33,5 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    {
-      resolve: "gatsby-source-graphql",
-      options: {
-        // Arbitrary name for the remote schema Query type
-        typeName: "django",
-        // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
-        fieldName: "DJANGO",
-        // Url or Django Graphene Endpoint
-        url: "http://127.0.0.1:8000/",
-      },
-    },
   ],
 }
